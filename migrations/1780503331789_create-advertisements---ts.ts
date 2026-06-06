81e532155db78d1891b5d0c6ef3bec1faef638d6
vadimvasilev <mrvasilievv@gmail.com>
2026-06-06 15:47:33 +0300

    feat: add auth middleware and user name update; add offers, categories, statuses tables

--- migrations/1780503331789_create-advertisements---ts.ts
+++ migrations/1780503331789_create-advertisements---ts.ts
@@ -14,10 +14,10 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 
     
     pgm.addConstraint('offers', 'fk_category', {
-        foreignKeys: { columns: 'category_id', references: 'categories(category_id)', onDelete: 'RESTRICT' }
+        foreignKeys: { columns: 'category_id', references: 'ad_categories(category_id)', onDelete: 'RESTRICT' }
     });
     pgm.addConstraint('offers', 'fk_status', {
-        foreignKeys: { columns: 'status_id', references: 'statuses(status_id)', onDelete: 'RESTRICT' }
+        foreignKeys: { columns: 'status_id', references: 'ad_statuses(status_id)', onDelete: 'RESTRICT' }
     });
     pgm.addConstraint('offers', 'fk_user', {
         foreignKeys: { columns: 'user_id', references: 'users(id)', onDelete: 'CASCADE' }
